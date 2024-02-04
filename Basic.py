fd9bd9d83df96b5a04f6de799931c214869a713d
Caden McArthur <cadenamcarthur@bethelks.edu>
2024-02-04 12:14:05 -0600

    2/4

--- Basic.py
+++ Basic.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-def main():
-    weather = pd.read_csv("TUL.csv", index_col="DATE")
+weather = pd.read_csv("newkirk.csv", index_col="DATE")
 
-    weather.loc["2020-1-1:2020-1-5",:]
-main()
+
